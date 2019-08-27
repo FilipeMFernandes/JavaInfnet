16f2af72d172270facbefe522fb9fae431289570
FilipeMFernandes <filipemateusreis@gmail.com>
2019-08-27 16:33:10 -0300

    Initial application generated by JHipster-6.2.0

---
+++ FinalProject/src/main/java/com/project/app/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.project.app.repository.search;
+
+import com.project.app.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
