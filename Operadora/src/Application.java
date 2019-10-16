0d717a607a633d44dddaecee6a454c20cb64bdb7
FilipeMFernandes <filipemateusreis@gmail.com>
2019-10-16 18:06:54 -0300

    a

--- Operadora/src/Application.java
+++ Operadora/src/Application.java
@@ -4,19 +4,24 @@ import domain.PessoaJuridica;
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 
 public class Application {
     public static void main(String[] args) {
         try {
             //pegando clientes do arquivo
 
-            File clientesFile = new File("/home/filipe/JavaInfnet/Operadora/src/resources/clientes");
-            BufferedReader reader = new BufferedReader(new FileReader(clientesFile));
+            File file = new File(".resources/clientes");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             String iterating;
             ArrayList<String> clientesStrings = new ArrayList<>();
             ArrayList<PessoaFisica> pessoasFisicas = new ArrayList<>();
             ArrayList<PessoaJuridica> pessoasJuridicas = new ArrayList<>();
             while ((iterating = reader.readLine()) != null){
+                String[] stringSplit = iterating.split(";");
                 clientesStrings.add(iterating);
             }
             buildClients(clientesStrings, pessoasFisicas, pessoasJuridicas);
@@ -24,13 +29,13 @@ public class Application {
             while(!sair) {
                 switch (menu()) {
                     case 1:
-                        criar(pessoasFisicas, pessoasJuridicas);
+                        criar(writer, pessoasFisicas, pessoasJuridicas);
                         break;
                     case 2:
-                        alterar(pessoasFisicas, pessoasJuridicas);
+//                        alterar(clientesFile, pessoasFisicas, pessoasJuridicas);
                         break;
                     case 3:
-                        excluir(pessoasFisicas, pessoasJuridicas);
+//                        excluir(clientesFile, pessoasFisicas, pessoasJuridicas);
                         break;
                     case 4:
                         relatorio(leOpcaoConsulta(), pessoasFisicas, pessoasJuridicas);
@@ -48,6 +53,34 @@ public class Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void criar(BufferedWriter writer, ArrayList<PessoaFisica> pessoasFisicas, ArrayList<PessoaJuridica> pessoasJuridicas) {
+        switch(JOptionPane.showInputDialog("Insira 1 para criar uma pessoa Fisica ou insira 2 para criar uma pessoa Juridica:")){
+            case"1":
+                String numero = leiaNumero();
+                String nome = leiaNome(1);
+        }
+
+
+    }
+
+    private static String leiaNome(int tipoPessoa) {
+        if(tipoPessoa == 1){
+            return JOptionPane.showInputDialog("Insira o nome do cliente:");
+        }else if(tipoPessoa == 2){
+            return JOptionPane.showInputDialog("Insira a razáo social do cliente:");
+        }
+//        throw new Exception("Something unexpected happened, call the developers");
+        return "";
+    }
+
+    private static String leiaNumero() {
+        String numero = JOptionPane.showInputDialog("Insira o numero do cliente:");
+        while(numero.length() != 8){
+            numero = JOptionPane.showInputDialog("O numero deve ter 8 digitos! Insira novamente:");
+        }
+        return numero;
     }
 
     private static void buildClients(ArrayList<String> clientes, ArrayList<PessoaFisica> pessoasFisicas, ArrayList<PessoaJuridica> pessoasJuridicas) {
@@ -97,16 +130,16 @@ public class Application {
     private static void relatorio(int opcao, ArrayList<PessoaFisica> pessoasFisicas, ArrayList<PessoaJuridica> pessoasJuridicas) {
         switch(opcao){
             case 1:
-                consultarTodos(pessoasFisicas, pessoasJuridicas);
+//                consultarTodos(pessoasFisicas, pessoasJuridicas);
                 break;
             case 2:
-                consultarSaldoZero(pessoasFisicas, pessoasJuridicas);
+//                consultarSaldoZero(pessoasFisicas, pessoasJuridicas);
                 break;
             case 3:
-                consultarPorSaldoX(pessoasFisicas, pessoasJuridicas);
+//                consultarPorSaldoX(pessoasFisicas, pessoasJuridicas);
                 break;
             case 4:
-                consultarMaiorSaldo(pessoasFisicas, pessoasJuridicas);
+//                consultarMaiorSaldo(pessoasFisicas, pessoasJuridicas);
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Opcao Invalida! Tente novamente.");
